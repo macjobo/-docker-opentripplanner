ebafa638f771fa0b32cb5c13e5dca2aec18deb66
Johann Grambichler <j.grambichler@conova.com>
2019-11-29 12:54:59 +0100

    runsh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java -Xmx1024m -jar /deployments/otp.jar --build /data --analyst --inMemory
+exec java -Xmx1024m -jar /deployments/otp.jar --build /deployments --analyst --inMemory
